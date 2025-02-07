c6a91c7c7c4051d9dca1b2502596d284f9706fc7
Don <100505855+DonFungible@users.noreply.github.com>
2025-02-06 16:34:53 -0800

    🔒️ Fix Sentry config

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -2,12 +2,13 @@
 // The config you add here will be used whenever one of the edge features is loaded.
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://a40b3167ea6c38875cd024822a8d5b3a@o4507534168031232.ingest.us.sentry.io/4507539422183424',
+  dsn: 'https://095492069cb7b9eaf7d94219f4933bfe@o4508775095140352.ingest.us.sentry.io/4508775096188928',
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,17 +1,15 @@
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://a40b3167ea6c38875cd024822a8d5b3a@o4507534168031232.ingest.us.sentry.io/4507539422183424',
+  dsn: 'https://095492069cb7b9eaf7d94219f4933bfe@o4508775095140352.ingest.us.sentry.io/4508775096188928',
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
 })
